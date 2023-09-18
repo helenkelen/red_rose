7a64ad75d184a202a7e4b583b14e242b763d1da8
Helen <helen601.639@gmail.com>
2023-09-18 17:09:02 +0300

    add logic

--- p.py
+++ p.py
@@ -13,3 +13,16 @@ if 5 > 2:
 #you need input password
 #if flour is open you need show it user and if close too
 
+#User write chooce number 
+usernumber = input("Write number your flour.")
+print("Your choice is: " + usernumber)
+
+#if, elif for user flour
+if usernumber > 0 and usernumber < 9:
+    print("Welcome!")
+
+elif usernumber == 10:
+    print("Please enter your code: ")
+
+else:
+    ("Sorry, uncorrect enter!")
