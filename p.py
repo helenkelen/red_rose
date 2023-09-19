95ad1edcf1a98e3690173743c8bf1441c74e88c1
Helen <helen601.639@gmail.com>
2023-09-19 14:39:35 +0300

    refresh

--- p.py
+++ p.py
@@ -10,43 +10,51 @@ def name():
     e3_n = "Helen Kolrum"
     e4_n = "Lucifer Morningstar"
 
+    print("Name employees: ")
     print("Hello " + e1_n)
     print("Hello " + e2_n)
     print("Hello " + e3_n)
     print("Hello " + e4_n)
+    print(" ")
 
 def age():
-    e1_a = 23
-    e2_a = 48
-    e3_a = 35
-    e4_a = 50
+    e1_a = "23"
+    e2_a = "48"
+    e3_a = "35"
+    e4_a = "50"
 
+    print("Age employees: ")
     print("Derek Patison: " + e1_a)
     print("Mark Tolstoy: " + e2_a)
     print("Helen Kolrum: " + e3_a)
     print("Lucifer Morningstar: " + e4_a)
+    print(" ")
 
 def born_date():
-    bd_1 = 23/11/1999
-    bd_2 = 4/3/2002
-    bd_3 = 5/6/1990
-    bd_4 = 7/7/1997
+    bd_1 = "23/11/1999"
+    bd_2 = "4/3/2002"
+    bd_3 = "5/6/1990"
+    bd_4 = "7/7/1997"
 
+    print("Born date employees: ")
     print("Derek Patison: " + bd_1)
     print("Mark Tolstoy: " + bd_2)
     print("Helen Kolrum: " + bd_3)
     print("Lucifer Morningstar: " + bd_4)
+    print(" ")
 
 def salary():
-    e1_s = 20000
-    e2_s = 5000
-    e3_s = 70000
-    e4_s = 34567
-    
+    e1_s = "20000"
+    e2_s = "5000"
+    e3_s = "70000"
+    e4_s = "34567"
+
+    print("Salary employees: ") 
     print("Derek Patison: " + e1_s)
     print("Mark Tolstoy: " + e2_s)
     print("Helen Kolrum: " + e3_s)
     print("Lucifer Morningstar: " + e4_s)
+    print(" ")
 
 def work():
     e1_w = "Manager"
@@ -54,11 +62,13 @@ def work():
     e3_w = "Cleaner"
     e4_w = "Waiter"
     
+    print("Work employees: ")
     print("Derek Patison: " + e1_w)
     print("Mark Tolstoy: " + e2_w)
     print("Helen Kolrum: " + e3_w)
     print("Lucifer Morningstar: " + e4_w)
-
+    print(" ")
+    
 name()
 age()
 born_date()
