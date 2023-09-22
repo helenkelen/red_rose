7d99265db2fb6460def3cb291f343e52e2243ab6
Helen <helen601.639@gmail.com>
2023-09-22 17:01:36 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,8 +1,31 @@
 #show user text for add first number
 print("Enter first number")
 
+#get first user number
+firstusernumber = int(input())
+
 #user choose do
 print("Enter +, -, *, /")
 
+#operator
+useroperator = input()
+
 #show user text for add second number
 print("Enter second number")
+
+#get second user number
+secondusernumber = int(input())
+
+if(useroperator == "+"):
+    adding = firstusernumber + secondusernumber
+    print(adding)
+elif(useroperator == "-"):
+    subt = firstusernumber - secondusernumber
+    print(subt)
+elif(useroperator == "*"):
+    mult = firstusernumber * secondusernumber
+    print(mult)
+elif(useroperator == "/"):
+    div = firstusernumber / secondusernumber
+    print(div)
+    
