98ae17b0d63bb7c5cfdd308243501778aaabaf68
Helen <helen601.639@gmail.com>
2023-09-22 16:33:44 +0300

    Update p.py

---
+++ p.py
@@ -0,0 +1,8 @@
+#show user text for add first number
+print("Enter first number")
+
+#user choose do
+print("Enter +, -, *, /")
+
+#show user text for add second number
+print("Enter second number")
